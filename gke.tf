11447169ae082bf947ae861e0da3b520957e13ed
lychees40 <lychees.gaseous-0q@icloud.com>
2024-12-09 12:06:16 +0700

    Update GKE configuration to enhance autoscaling, increase node pool limits, and improve monitoring settings

--- gke.tf
+++ gke.tf
@@ -1,36 +1,51 @@
 // GKE Cluster
 module "gke" {
-  source                               = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster"
-  version                              = "34.0.0"
-  project_id                           = var.project_id
-  name                                 = "${var.name}-${var.env}-gke"
-  region                               = var.region
-  zones                                = ["${var.region}-a"]
-  network                              = module.vpc.network_name
-  subnetwork                           = module.vpc.subnets_names[0]
-  ip_range_pods                        = "gke-pods"
-  ip_range_services                    = "gke-services"
-  horizontal_pod_autoscaling           = true
-  enable_private_nodes                 = true
-  master_ipv4_cidr_block               = var.gke_master_ipv4_cidr_block
-  dns_cache                            = false
-  add_cluster_firewall_rules           = true
-  add_master_webhook_firewall_rules    = true
-  add_shadow_firewall_rules            = true
-  deletion_protection                  = false
-  remove_default_node_pool             = true
-  disable_legacy_metadata_endpoints    = true
-  network_policy                       = true
-  enable_intranode_visibility          = true
-  gateway_api_channel                  = "CHANNEL_STANDARD"
-  security_posture_mode                = "BASIC"
-  security_posture_vulnerability_mode  = "VULNERABILITY_BASIC"
-  release_channel                      = "STABLE"
-  kubernetes_version                   = "1.30.5-gke.1443001"
+  source            = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster-update-variant"
+  version           = "34.0.0"
+  project_id        = var.project_id
+  name              = "${var.name}-${var.env}-gke"
+  region            = var.region
+  zones             = ["${var.region}-a"]
+  network           = module.vpc.network_name
+  subnetwork        = module.vpc.subnets_names[0]
+  ip_range_pods     = "gke-pods"
+  ip_range_services = "gke-services"
+
+  horizontal_pod_autoscaling      = true
+  enable_vertical_pod_autoscaling = true
+
+  enable_private_nodes              = true
+  master_ipv4_cidr_block            = var.gke_master_ipv4_cidr_block
+  dns_cache                         = false
+  add_cluster_firewall_rules        = true
+  add_master_webhook_firewall_rules = true
+  add_shadow_firewall_rules         = true
+  deletion_protection               = false
+
+  remove_default_node_pool          = true
+  disable_legacy_metadata_endpoints = true
+  network_policy                    = true
+
+  gateway_api_channel                 = "CHANNEL_STANDARD"
+  security_posture_mode               = "BASIC"
+  security_posture_vulnerability_mode = "VULNERABILITY_BASIC"
+  release_channel                     = "STABLE"
+  kubernetes_version                  = "1.30.5-gke.1443001"
+
   monitoring_enable_managed_prometheus = true
-  enable_secret_manager_addon          = true
-  workload_vulnerability_mode          = "BASIC"
-  workload_config_audit_mode           = "BASIC"
+  monitoring_enabled_components        = ["SYSTEM_COMPONENTS"]
+  monitoring_service                   = "monitoring.googleapis.com/kubernetes"
+
+  logging_enabled_components = ["SYSTEM_COMPONENTS"]
+  logging_service            = "logging.googleapis.com/kubernetes"
+
+
+  # enable_secret_manager_addon = true
+  # workload_vulnerability_mode = "BASIC"
+  # workload_config_audit_mode  = "BASIC"
+  # enable_intranode_visibility = true
+
+  # }
   // Open gke control plane API conditionally
   enable_private_endpoint = var.control_plane_open ? false : true
   master_authorized_networks = var.control_plane_open ? [
@@ -41,6 +56,7 @@ module "gke" {
     }
   ] : []
   depends_on = [module.vpc]
+
   node_pools = [
     {
       name = "${var.name}${var.env}-node-pool"
@@ -48,8 +64,8 @@ module "gke" {
       machine_type         = "e2-standard-2"
       node_locations       = "${var.region}-a"
       min_count            = 1
-      max_count            = 2
-      disk_size_gb         = 40
+      max_count            = 6
+      disk_size_gb         = 50
       disk_type            = "pd-ssd"
       kubernetes_version   = "1.30.5-gke.1443001"
       auto_repair          = true
@@ -58,7 +74,7 @@ module "gke" {
       initial_node_count   = 1
       enable_private_nodes = true
       logging_variant      = "DEFAULT"
-
+      strategy             = "BLUE_GREEN"
     },
   ]
 
